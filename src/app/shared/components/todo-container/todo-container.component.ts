e5f95a09aa67e17a7dc83a0bd4500929652a1d20
matink714 <matink714@gmail.com>
2025-04-22 11:06:45 +0330

    feat: create todo container component

---
+++ src/app/shared/components/todo-container/todo-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-container',
+  imports: [],
+  templateUrl: './todo-container.component.html',
+  styleUrl: './todo-container.component.scss'
+})
+export class TodoContainerComponent {
+
+}
