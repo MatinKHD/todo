9e0ea47b8fc232fad2bd97cbe13e695ee0f6834d
matink714 <matink714@gmail.com>
2025-04-22 11:47:31 +0330

    fix: loading components in route.ts and add redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,23 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadChildren: () => import('./features/layout/layout.component').then(m => m.LayoutComponent),
+        loadComponent: () => import('./features/layout/layout.component').then(m => m.LayoutComponent),
         children: [
             {
                 path: 'home',
-                loadChildren: () => import('./features/home/home.component').then(m => m.HomeComponent),
+                loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent),
             },
             {
                 path: 'completed',
-                loadChildren: () => import('./features/completed/completed.component').then(m => m.CompletedComponent),
+                loadComponent: () => import('./features/completed/completed.component').then(m => m.CompletedComponent),
             },
             {
                 path: 'list/:id',
-                loadChildren: () => import('./features/list/list.component').then(m => m.ListComponent),
+                loadComponent: () => import('./features/list/list.component').then(m => m.ListComponent),
+            },
+            {
+                path: '**',
+                redirectTo: 'home',
             }
         ]
     }
