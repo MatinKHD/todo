9ac880597247d3fa6cb28aa5fa202d8ccff1199c
matink714 <matink714@gmail.com>
2025-04-22 12:47:14 +0330

    cheat: implement static side nav in layout component

--- src/app/features/layout/layout.component.ts
+++ src/app/features/layout/layout.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
+import { LocalRepository } from '../../core/local-store/local-repository';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    NgIf,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  isDesktop = true;
 
+  constructor(
+    private _localRepository: LocalRepository,
+  ) {
+
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    if (!this._localRepository.IsInBrowser) return;
+    this.isDesktop = window.innerWidth > 768;
+  }
+
+  ngOnInit(): void {
+    if (!this._localRepository.IsInBrowser) return;
+    this.isDesktop = window.innerWidth > 768;
+  }
 }
