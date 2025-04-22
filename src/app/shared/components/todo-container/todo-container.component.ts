7b9038d7b118e46fd7b41ef1b1224104dd0cd8ae
matink714 <matink714@gmail.com>
2025-04-22 18:28:50 +0330

    cheat: implement todo container with static data

--- src/app/shared/components/todo-container/todo-container.component.ts
+++ src/app/shared/components/todo-container/todo-container.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TodoComponent } from '../todo/todo.component';
 
 @Component({
   selector: 'app-todo-container',
-  imports: [],
+  imports: [
+    TodoComponent
+  ],
   templateUrl: './todo-container.component.html',
   styleUrl: './todo-container.component.scss'
 })
 export class TodoContainerComponent {
+  @Input() title: string = 'title';
+  @Input() date: Date = new Date();
+  @Input() description: string = 'description descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription';
+  @Input() isCompleted: boolean = false;
+
+  @Output() delete = new EventEmitter<void>();
+  @Output() edit = new EventEmitter<void>();
+  @Output() markComplete = new EventEmitter<boolean>();
+
+  onDeleteTodo() {
+    this.delete.emit();
+  }
+
+  onEditTodo() {
+    this.edit.emit();
+  }
+
+  onMarkTodoComplete(event: any) {
+    this.isCompleted = event.checked;
+    this.markComplete.emit(this.isCompleted);
+  }
 
 }
