d88ab5e1433b2c9f282ab136f037ea9185a35898
matink714 <matink714@gmail.com>
2025-04-22 18:29:49 +0330

    cheat: implement home compnent with static data

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TodoContainerComponent } from '../../shared/components/todo-container/todo-container.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    TodoContainerComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
