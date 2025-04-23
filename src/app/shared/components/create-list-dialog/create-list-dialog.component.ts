7144a45ae51c27f5e6e716cdccfc4b4e5bcd4e34
matink714 <matink714@gmail.com>
2025-04-23 14:36:31 +0330

    cheat: implement create-list-dialog component

--- src/app/shared/components/create-list-dialog/create-list-dialog.component.ts
+++ src/app/shared/components/create-list-dialog/create-list-dialog.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { DialogRef } from '@angular/cdk/dialog';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create-list-dialog',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
+  ],
   templateUrl: './create-list-dialog.component.html',
   styleUrl: './create-list-dialog.component.scss'
 })
 export class CreateListDialogComponent {
+
+  title!: string;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<CreateListDialogComponent>,
+  ) {
+    this.title = data.isEditable ? data.title : '' ;
+  }
+  
+  handleClose(data: string | null = null) {
+    this.dialogRef.close(data);
+  }
+
+  handleSubmit() {
+    this.handleClose(this.title);
+  }
 
 }
