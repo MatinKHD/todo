65a783fafd589041cc7298d92ccc7e156f74688c
matink714 <matink714@gmail.com>
2025-04-23 14:00:32 +0330

    feat: create create-list-dialog

---
+++ src/app/shared/components/create-list-dialog/create-list-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-list-dialog',
+  imports: [],
+  templateUrl: './create-list-dialog.component.html',
+  styleUrl: './create-list-dialog.component.scss'
+})
+export class CreateListDialogComponent {
+
+}
