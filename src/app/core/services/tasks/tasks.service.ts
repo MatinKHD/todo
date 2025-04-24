ddc774250dd5cf0235f1cf41fa7e3c974ff8f245
matink714 <matink714@gmail.com>
2025-04-24 12:25:04 +0330

    cheat: implement task service

--- src/app/core/services/tasks/tasks.service.ts
+++ src/app/core/services/tasks/tasks.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { BaseService } from '../base.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TaskInterface } from '../../../shared/interfaces/task.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TasksService {
+export class TasksService extends BaseService {
 
-  constructor() { }
+  constructor(http: HttpClient) { 
+    super(http)
+  }
+
+  getAllTasks(): Observable<TaskInterface[]> {
+    return this.getAll<TaskInterface[]>('tasks');
+  }
+
+  findTaskByListId(listId: string): Observable<TaskInterface> {
+    return this.getByDetail<TaskInterface>('tasks/query/', listId);
+  }
+
+  getTaskById(id: string): Observable<Omit<TaskInterface, 'id'>> {
+    return this.getByDetail<Omit<TaskInterface, 'id'>>('tasks/', id);
+  }
+
+  updateTask(id:string, body: Omit<TaskInterface, 'id'>): Observable<TaskInterface> {
+    return this.put<TaskInterface, Omit<TaskInterface, 'id'>>('tasks/', body, id);
+  }
+
+  createTask( body: Omit<TaskInterface, 'id'>): Observable<TaskInterface> {
+    return this.post<TaskInterface, Omit<TaskInterface, 'id'>>('tasks/', body);
+  }
+
+  deleteTask(id: string): Observable<TaskInterface> {
+    return this.delete<TaskInterface>('tasks/', id);
+  }
 }
