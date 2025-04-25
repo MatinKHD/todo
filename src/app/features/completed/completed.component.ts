d5df4a5cdd508c244cc0635598206d25fe4c546e
matink714 <matink714@gmail.com>
2025-04-25 17:16:52 +0330

    cheat: implement completed component

--- src/app/features/completed/completed.component.ts
+++ src/app/features/completed/completed.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { BaseComponent } from '../../core/components/base.component';
+import { TasksService } from '../../core/services/tasks/tasks.service';
+import { TaskInterface } from '../../shared/interfaces/task.interface';
+import { tap, Observable } from 'rxjs';
+import { TodoContainerComponent } from '../../shared/components/todo-container/todo-container.component';
+import { BaseTaskComponent } from '../../core/components/base-task.component';
 
 @Component({
   selector: 'app-completed',
-  imports: [],
+  imports: [
+    TodoContainerComponent
+  ],
   templateUrl: './completed.component.html',
   styleUrl: './completed.component.scss'
 })
-export class CompletedComponent {
+export class CompletedComponent extends BaseTaskComponent {
+
+  override getTasks(): Observable<TaskInterface[]> {
+    return this.tasksService.getCompletedTasks();
+  }
 
 }
