03487b6e4f62a86e1660d6003b6f4fbfed37889b
matink714 <matink714@gmail.com>
2025-04-25 17:16:28 +0330

    cheat: change implementation of home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -4,6 +4,7 @@ import { BaseComponent } from '../../core/components/base.component';
 import { TodoContainerComponent } from '../../shared/components/todo-container/todo-container.component';
 import { TaskInterface } from '../../shared/interfaces/task.interface';
 import { TasksService } from './../../core/services/tasks/tasks.service';
+import { BaseTaskComponent } from '../../core/components/base-task.component';
 
 @Component({
   selector: 'app-home',
@@ -13,56 +14,9 @@ import { TasksService } from './../../core/services/tasks/tasks.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent extends BaseComponent implements OnInit {
+export class HomeComponent extends BaseTaskComponent {
 
-  tasksService = inject(TasksService);
-  dailyTasks = signal<TaskInterface[]>([]);
-
-  ngOnInit(): void {
-    const sub = this.getAllDailyTasks().subscribe();
-    this.subscriptions.push(sub);
+  override getTasks(): Observable<TaskInterface[]> {
+    return this.tasksService.getDailyTasks();
   }
-
-  onDeleteTodo(id: string) {
-    this.tasksService.deleteTask(id).pipe(
-      tap(() => this.dailyTasks.update(tasks => tasks.filter((task) => task.id !== id)))
-    ).subscribe();
-  }
-
-  onMarkTodoComplete(task: TaskInterface) {
-    this.tasksService.updateTask(task.id, task).pipe(
-      tap(() => {
-        this.dailyTasks.update((tasks) => {
-          const taskIndex = tasks.findIndex(t => t.id === task.id);
-          if (taskIndex !== -1) {
-            tasks[taskIndex] = { ...tasks[taskIndex], done: task.done }
-          }
-          return tasks;
-        })
-      }),
-      this.handleError('Something wrong, please try again')
-    ).subscribe();
-  }
-
-  
-  onEditTodo(task: TaskInterface) {
-    this.tasksService.updateTask(task.id, task).pipe(
-      tap(() => {
-        this.dailyTasks.update((tasks) => {
-          const taskIndex = tasks.findIndex(t => t.id === task.id);
-          tasks[taskIndex] = { ...task };
-          this.snackBar.notification$.next('Edited Successfully')
-          return tasks;
-        })
-      }),
-      this.handleError(`Can't edit`)
-    ).subscribe();
-  }
-
-  private getAllDailyTasks(): Observable<TaskInterface[]> {
-    return this.tasksService.getDailyTasks().pipe(
-      tap(res => this.dailyTasks.set(res))
-    )
-  }
-
 }
