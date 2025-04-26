8519432f6e7889bf0e54fd772d2bf1a722b7cb4b
matink714 <matink714@gmail.com>
2025-04-26 18:17:57 +0330

    cheat: override edit task

--- src/app/features/completed/completed.component.ts
+++ src/app/features/completed/completed.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, switchMap } from 'rxjs';
 import { BaseTaskComponent } from '../../core/components/base-task.component';
 import { TodoContainerComponent } from '../../shared/components/todo-container/todo-container.component';
 import { TaskInterface } from '../../shared/interfaces/task.interface';
+import { TaskDialogComponent } from '../../shared/components/task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-completed',
@@ -16,6 +17,22 @@ export class CompletedComponent extends BaseTaskComponent {
 
   override getTasks(): Observable<TaskInterface[]> {
     return this.tasksService.getCompletedTasks();
+  }
+  // override this method for letting user to edit the task without considering duplicate time 
+  override onEditTask(task: TaskInterface): void {
+    this.dialog.open(TaskDialogComponent, {
+      data: {
+        task,
+        isEditMode: true,
+        exisitngDates: []
+      }
+    }).afterClosed().pipe(
+      switchMap((res) => {
+        console.log(res);
+        
+        return res ? this.updateTask(res).pipe(this.handleError(`Something went wrong, Can't edit`)) : of(null);
+      })
+    ).subscribe();
   }
 
 }
