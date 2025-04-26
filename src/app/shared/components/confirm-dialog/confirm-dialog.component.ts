301aa74817ef04bf25821dff50901a500c0dbc31
matink714 <matink714@gmail.com>
2025-04-26 12:03:15 +0330

    cheat: implement confirm dialog

--- src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogContent, MatDialogActions, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-dialog',
-  imports: [],
+  imports: [MatDialogModule, MatButtonModule, MatDialogContent, MatDialogActions],
   templateUrl: './confirm-dialog.component.html',
-  styleUrl: './confirm-dialog.component.scss'
+  styleUrls: ['./confirm-dialog.component.scss']
 })
 export class ConfirmDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string }
+  ) {}
 
+  onNoClick(): void {
+    this.dialogRef.close(false); // Return `false` when "No" is clicked
+  }
+
+  onYesClick(): void {
+    this.dialogRef.close(true); // Return `true` when "Yes" is clicked
+  }
 }
