b9e15f480b967d2ed3ec7095224b0034c86a948a
matink714 <matink714@gmail.com>
2025-04-26 22:16:43 +0330

    cheat: implement home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,20 +1,38 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable, switchMap, tap } from 'rxjs';
 import { BaseTaskComponent } from '../../core/components/base-task.component';
 import { TodoContainerComponent } from '../../shared/components/todo-container/todo-container.component';
 import { TaskInterface } from '../../shared/interfaces/task.interface';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   imports: [
-    TodoContainerComponent
+    TodoContainerComponent,
+    MatIconModule, 
+    MatButtonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent extends BaseTaskComponent {
+export class HomeComponent extends BaseTaskComponent implements OnInit {
+
+  override ngOnInit(): void {
+    this.getMainList().pipe(
+      switchMap(() => this.getTasks()),
+      tap((res) => this.tasks.set(res)),
+      tap(() => this.existingDates.set(this.tasks().map((t) => t.date.toString()))),
+    ).subscribe();
+  }
+  
+  getMainList(): Observable<any> {
+    return this.listService.getDailyTaskListDetail().pipe(
+      tap(list => this.listDetail.set(list))
+    )
+  }
 
   override getTasks(): Observable<TaskInterface[]> {
-    return this.tasksService.getDailyTasks();
+    return this.tasksService.getTasksOfList(this.listDetail()?._id!);
   }
 }
