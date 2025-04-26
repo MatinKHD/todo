eacb8bf2f5f8876c3cc4988c08a78a96a6a8c90c
matink714 <matink714@gmail.com>
2025-04-26 16:09:37 +0330

    fix: change color for completed tasks

--- src/app/shared/pipes/date-color.pipe.ts
+++ src/app/shared/pipes/date-color.pipe.ts
@@ -4,12 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dateColor'
 })
 export class DateColorPipe implements PipeTransform {
-  transform(date: string | Date): string {
+  transform(date: string | Date, done: boolean): string {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
     const inputDate = new Date(date);
 
-    return inputDate < today ? 'text-error-50' : '';
+    return inputDate < today && !done ? 'text-error-50' : 'text-gray-500';
   }
 
 }
